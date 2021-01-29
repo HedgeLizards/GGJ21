5d270c05cfc8bf7ac7951083d16f61097d607615
troido <troido@protonmail.com>
2021-01-29 23:45:38 +0100

    make exported vars for canary constants

--- scripts/Canary.gd
+++ scripts/Canary.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+export var GRAVITY = 300
+export var speed = 200
+export var flap_speed = 350
 var active = false
 var vspeed = 0
 var is_player = true
@@ -8,19 +11,19 @@ func _physics_process(delta):
 	var hspeed = 0
 	
 	if Input.is_action_pressed('left') and active:
-		hspeed -= 200
+		hspeed -= speed
 	if Input.is_action_pressed('right')and active:
-		hspeed += 200
+		hspeed += speed
 	
 	if Input.is_action_just_pressed('up') and active:
-		vspeed = -350
+		vspeed = -flap_speed
 	
 	move_and_slide(Vector2(hspeed, vspeed), Vector2.UP)
 	
 	if is_on_floor() || is_on_ceiling():
 		vspeed = 5
 	
-	vspeed += 300 * delta
+	vspeed += GRAVITY * delta
 	
 	if is_on_floor():
 		$Label.text = 'IDLE'
