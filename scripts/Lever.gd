546bc3f83b424f19b5ba1c56d3c71b07456982a2
troido <troido@protonmail.com>
2021-01-29 23:49:03 +0100

    only active player can pull levers

--- scripts/Lever.gd
+++ scripts/Lever.gd
@@ -7,7 +7,7 @@ signal on_pulled
 func _input(event):
 	if Input.is_action_just_pressed("interact"):
 		for body in get_overlapping_bodies():
-			if body.get("is_player") != null:
+			if body.get("is_player") != null and body.active:
 				emit_signal("on_pulled")
 				break
 
