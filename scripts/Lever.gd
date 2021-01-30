4ff640cf346dd6c818d4d1a5e2f0b702a7d93b1e
troido <troido@protonmail.com>
2021-01-30 23:02:47 +0100

    add lever animation

--- scripts/Lever.gd
+++ scripts/Lever.gd
@@ -5,9 +5,10 @@ signal on_pulled
 
 
 func _input(event):
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact") and not $PullAnimation.is_playing():
 		for body in get_overlapping_bodies():
 			if body.get("is_player") != null and body.active:
 				emit_signal("on_pulled")
+				$PullAnimation.play("pull_lever")
 				break
 
