c55c5f99e8a7c288061f82b30aa887ec5a73e928
Julian Vos <julian.vos@ixly.nl>
2021-01-30 11:52:26 +0100

    Improved the Valve script

--- scripts/Valve.gd
+++ scripts/Valve.gd
@@ -7,25 +7,25 @@ onready var color_rect = get_node('%s/ColorRect' % water)
 onready var narrowness = color_rect.material.get_shader_param('narrowness')
 onready var particles_2d = get_node('%s/Particles2D' % water)
 
-var activatable = false
+var activator = null
 var open = true
 var animation = 0
 
 func _on_Valve_body_entered(body):
-	activatable = true
+	activator = body
 
 func _on_Valve_body_exited(body):
-	activatable = false
+	activator = null
 
 func _unhandled_input(event):
-	if activatable && event.is_action_pressed('interact'):
+	if activator && activator.active && event.is_action_pressed('interact'):
+		collision_shape_2d.disabled = open
+		
 		open = !open
 		
 		particles_2d.emitting = open
 		
 		if open:
-			collision_shape_2d.disabled = false
-			
 			$Tween.interpolate_property(self, 'animation', null, 0, animation * 2, Tween.EASE_IN_OUT, Tween.TRANS_SINE)
 		else:
 			$Tween.interpolate_property(self, 'animation', null, 1, (1 - animation) * 2, Tween.EASE_IN_OUT, Tween.TRANS_SINE)
@@ -36,6 +36,3 @@ func _on_Tween_tween_step(object, key, elapsed, value):
 	rotation_degrees = animation * 270
 	
 	color_rect.material.set_shader_param('narrowness', narrowness + (0.5 - narrowness) * animation)
-
-func _on_Tween_tween_completed(object, key):
-	collision_shape_2d.disabled = true
