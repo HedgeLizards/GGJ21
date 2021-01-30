4edd15e06516136ba603aa2fa8418cedd96fa25a
troido <troido@protonmail.com>
2021-01-30 22:40:45 +0100

    lightning the torch

--- scripts/Lantern.gd
+++ scripts/Lantern.gd
@@ -14,6 +14,13 @@ export var max_energy = 1.5
 var t = 0
 
 
+enum {OFF, LIGHTING, ON}
+var alight = OFF
+
+
+func _ready():
+	texture_scale = 0
+	lightup()
 
 func _process(delta):
 	t += delta
@@ -22,3 +29,18 @@ func _process(delta):
 	energy += delta * ((1-avg_attraction) * rand_range(-d_energy, d_energy) + avg_attraction * (avg_energy - energy))
 	energy = clamp(energy, min_energy, max_energy)
 	rotation = sin(1+(t*2) * (2*PI) / swing_time) * rotation_factor
+
+
+func lightup():
+	if alight == OFF:
+		$LighterSound.play()
+		$PreLight.start()
+	
+func start_light():
+	$Tween.interpolate_property(self, 'energy', 0, avg_energy, 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property(self, 'texture_scale', 0, 1, 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+
+func _on_Tween_tween_all_completed():
+	alight = ON
