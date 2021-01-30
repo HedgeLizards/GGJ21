f3b8865672c22c8409b9b293536d3b8d26233c76
troido <troido@protonmail.com>
2021-01-30 13:20:43 +0100

    decreased swing

--- scripts/Lantern.gd
+++ scripts/Lantern.gd
@@ -1,8 +1,9 @@
 extends Light2D
 
 
-export var swing = 10
+export var swing = 6
 export var swing_time = 3
+
 export var d_energy = 0.8
 export var avg_energy = 1.0
 export var avg_attraction = 0.1
@@ -14,7 +15,7 @@ var t = 0
 
 func _process(delta):
 	t += delta
-	position.x = sin(t * (2*PI) / swing_time) * swing + rand_range(-1.0, 1.0)
-	position.y = -sin((t*2) * (2*PI) / swing_time) * swing + rand_range(-1.0, 1.0)
+	position.x = sin(t * (2*PI) / swing_time) * swing 
+	position.y = -sin((t*2) * (2*PI) / swing_time) * swing 
 	energy += delta * ((1-avg_attraction) * rand_range(-d_energy, d_energy) + avg_attraction * (avg_energy - energy))
 	energy = clamp(energy, min_energy, max_energy)
