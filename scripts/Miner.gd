232f7635a2269ea46e91a8a9c2540e5a2745b2b3
troido <troido@protonmail.com>
2021-01-30 14:08:53 +0100

    elevator platform now a kinematicbody

--- scripts/Miner.gd
+++ scripts/Miner.gd
@@ -8,8 +8,6 @@ var vel = Vector2()
 var active = true
 var is_player = true
 
-# adapted from 3d thing
-
 func _physics_process(delta):
 	if active:
 		var input_movement = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
@@ -18,12 +16,10 @@ func _physics_process(delta):
 		vel.x = 0
 	
 	vel.y += delta * GRAVITY
-	if is_on_floor() or jump_in_air:
+	if is_on_floor():
 		if Input.is_action_just_pressed("up") and active:
 			vel.y = -jump_speed
 	
 	var oldvel = vel
 	vel = self.move_and_slide(vel, Vector2(0, -1), true, 4, deg2rad(60))
-	
-	
 
