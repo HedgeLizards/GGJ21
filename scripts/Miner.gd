a2028418e97cf762fab1b2201a91bb89c1cdf7fb
troido <troido@protonmail.com>
2021-01-31 10:52:40 +0100

    fix elevator

--- scripts/Miner.gd
+++ scripts/Miner.gd
@@ -7,6 +7,12 @@ export var jump_in_air = false
 var vel = Vector2()
 var active = true
 var is_player = true
+var standing = true
+
+var oldpos = Vector2()
+
+func _ready():
+	oldpos = position
 
 func _physics_process(delta):
 	if active:
@@ -21,7 +27,7 @@ func _physics_process(delta):
 			$Tween.interpolate_property($Hand, 'position:x', null, -$Hand.position.x, 0.2, Tween.TRANS_SINE, Tween.EASE_OUT)
 			$Tween.start()
 		
-		if input_movement != 0 && is_on_floor():
+		if input_movement != 0 && (is_on_floor() or standing):
 			$Sprite.playing = true
 		else:
 			$Sprite.playing = false
@@ -34,6 +40,14 @@ func _physics_process(delta):
 	vel.y += delta * GRAVITY
 	if is_on_floor():
 		if Input.is_action_just_pressed("up") and active:
+			standing = false
 			vel.y = -jump_speed
 	
-	vel = self.move_and_slide(vel, Vector2.UP)
+	var dvel = self.move_and_slide(vel, Vector2(0, -1))
+	vel = dvel
+	
+	if is_on_floor():
+		standing = true
+	elif position.y - oldpos.y > 0:
+		standing = false
+	oldpos = position
